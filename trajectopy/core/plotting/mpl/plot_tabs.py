Bug fix: rotation DOFs were plotted in radians instead of degree

trajectopy/core/plotting/mpl/plot_tabs.py
@@ -338,7 +338,7 @@ class PlotTabs(QtWidgets.QMainWindow):
                         PlotableDropdownItem(
                             name="Roll",
                             data=ate_result.trajectory.pos.xyz,
-                            color_data=ate_result.rot_dev_x,
+                            color_data=np.rad2deg(ate_result.rot_dev_x),
                             colorbar_label="Deviation [°]",
                             x_label=x_label,
                             y_label=y_label,
@@ -348,7 +348,7 @@ class PlotTabs(QtWidgets.QMainWindow):
                         PlotableDropdownItem(
                             name="Pitch",
                             data=ate_result.trajectory.pos.xyz,
-                            color_data=ate_result.rot_dev_y,
+                            color_data=np.rad2deg(ate_result.rot_dev_y),
                             colorbar_label="Deviation [°]",
                             x_label=x_label,
                             y_label=y_label,
@@ -358,7 +358,7 @@ class PlotTabs(QtWidgets.QMainWindow):
                         PlotableDropdownItem(
                             name="Yaw",
                             data=ate_result.trajectory.pos.xyz,
-                            color_data=ate_result.rot_dev_z,
+                            color_data=np.rad2deg(ate_result.rot_dev_z),
                             colorbar_label="Deviation [°]",
                             x_label=x_label,
                             y_label=y_label,

trajectopy/core/plotting/mpl/results.py
@@ -235,7 +235,7 @@ def colored_scatter_plot(
 
 def _setup_cbar_params(c_list, plot_settings: MPLPlotSettings):
     """Configures the colorbar ticks and labels for the scatter plot"""
-    if plot_settings.colorbar_max_std == 0:
+    if plot_settings.colorbar_max_std <= 0:
         lower_bound = np.min(c_list)
         upper_bound = np.max(c_list)
         geq_leq_dict = {0: "", plot_settings.colorbar_steps: ""}